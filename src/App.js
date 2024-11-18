46a2b55309b895de3cb0850f3abee003d3f4b2af
Denis Bekkulov <dionisvegan@icloud.com>
2024-11-18 23:23:18 +0300

    Start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Div = styled.div`
+	text-align: center;
+	font-weight: bold;
+	color: red;
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+	font-size: 24px;
+`;
 
-export default App;
+export const App = () => {
+	return (
+		<div className="App">
+			<Div>
+				<i className="fa fa-calendar"></i>Test styled component
+			</Div>
+		</div>
+	);
+};
